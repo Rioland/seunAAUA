8626d5ffe81ede7496f862a8e127a2047d4cf9e3
Rioland <33238630+Rioland@users.noreply.github.com>
2023-06-28 20:17:16 +0000

    adding template

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
 from flask import Flask
+from flask import render_template as render
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+def index():
+    return render("index.html")
+  
+@app.route("/about")
+def about():
+    return render("about.html")
+  
+@app.route("/cart")
+def cart():
+    return render("cart.html")
+  
+@app.route("/checkout")
+def checkout():
+    return render("checkout.html")
+
+
+@app.route("/contact")
+def contact():
+    return render("contact.html")
+  
+@app.route("/shop")
+def shop():
+    return render("shop.html")
+  
+@app.route("/single-product")
+def singlepage():
+    return render("single-product.html")
 
 
 if __name__=="__main__":
